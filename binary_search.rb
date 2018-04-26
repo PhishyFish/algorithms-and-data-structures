2fb2bd2a364a3a462566c04c1dcf53e6009fcaec
Vickie <9098023+PhishyFish@users.noreply.github.com>
2018-04-25 23:56:16 -0700

    Add recursive binary search (Ruby)

--- binary_search.rb
+++ binary_search.rb
@@ -16,3 +16,19 @@ def binary_search(arr, num)
 
   nil
 end
+
+def binary_search_recursive(arr, num)
+  return nil if arr.empty?
+
+  start = 0
+  stop = arr.length - 1
+  mid = (start + stop) / 2
+
+  if arr[mid] < num
+    mid + 1 + binary_search_recursive(arr[mid + 1..stop], num)
+  elsif arr[mid] > num
+    binary_search_recursive(arr[start..mid - 1], num)
+  else
+    mid
+  end
+end
