aebd4b54d0627622cd298da20b563a377c24c96c
Vickie <9098023+PhishyFish@users.noreply.github.com>
2018-04-27 01:13:52 -0700

    Add recursive binary search (JS)

--- binary_search.js
+++ binary_search.js
@@ -16,3 +16,21 @@ function binarySearch(arr, num) {
 
   return -1;
 }
+
+function binarySearchRecursive(arr, num) {
+  if (arr.length < 1) return -1;
+
+  let start = 0;
+  let stop = arr.length - 1;
+  let mid = Math.floor((start + stop) / 2);
+
+  if (arr[mid] < num) {
+    let result = binarySearchRecursive(arr.slice(mid + 1, stop + 1), num);
+    return result !== -1 ? mid + 1 + result : result;
+  }
+  if (arr[mid] > num) {
+    return binarySearchRecursive(arr.slice(start, mid), num);
+  }
+  
+  return mid;
+}
